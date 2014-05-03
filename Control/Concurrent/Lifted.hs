b788d3099490d5574647a671a5a613bf3f3aa1c2
minamiyama1994 <masakazu.minamiyama@gmail.com>
2014-05-04 08:48:18 +0900

    remove UnicodeSyntax Extention and dep of base-unicode-symbols

--- Control/Concurrent/Lifted.hs
+++ Control/Concurrent/Lifted.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE CPP, UnicodeSyntax, NoImplicitPrelude, FlexibleContexts, RankNTypes #-}
+{-# LANGUAGE CPP, NoImplicitPrelude, FlexibleContexts, RankNTypes #-}
 
 #if __GLASGOW_HASKELL__ >= 702
 {-# LANGUAGE Trustworthy #-}
@@ -101,7 +101,7 @@ import           Control.Concurrent ( ThreadId )
 import qualified Control.Concurrent as C
 
 -- from base-unicode-symbols:
-import Data.Function.Unicode ( (∘) )
+import Prelude ( (.) )
 
 -- from transformers-base:
 import Control.Monad.Base ( MonadBase, liftBase )
@@ -135,7 +135,7 @@ import Control.Exception.Lifted ( throwTo
 --------------------------------------------------------------------------------
 
 -- | Generalized version of 'C.myThreadId'.
-myThreadId ∷ MonadBase IO m ⇒ m ThreadId
+myThreadId :: MonadBase IO m => m ThreadId
 myThreadId = liftBase C.myThreadId
 {-# INLINABLE myThreadId #-}
 
@@ -144,7 +144,7 @@ myThreadId = liftBase C.myThreadId
 -- Note that, while the forked computation @m ()@ has access to the captured
 -- state, all its side-effects in @m@ are discarded. It is run only for its
 -- side-effects in 'IO'.
-fork ∷ MonadBaseControl IO m ⇒ m () → m ThreadId
+fork :: MonadBaseControl IO m => m () -> m ThreadId
 fork = liftBaseDiscard C.forkIO
 {-# INLINABLE fork #-}
 
@@ -154,9 +154,9 @@ fork = liftBaseDiscard C.forkIO
 -- Note that, while the forked computation @m ()@ has access to the captured
 -- state, all its side-effects in @m@ are discarded. It is run only for its
 -- side-effects in 'IO'.
-forkWithUnmask ∷ MonadBaseControl IO m ⇒ ((∀ a. m a → m a) → m ()) → m ThreadId
-forkWithUnmask f = liftBaseWith $ \runInIO →
-                     C.forkIOWithUnmask $ \unmask →
+forkWithUnmask :: MonadBaseControl IO m => ((∀ a. m a -> m a) -> m ()) -> m ThreadId
+forkWithUnmask f = liftBaseWith $ \runInIO ->
+                     C.forkIOWithUnmask $ \unmask ->
                        void $ runInIO $ f $ liftBaseOp_ unmask
 {-# INLINABLE forkWithUnmask #-}
 #endif
@@ -168,8 +168,8 @@ forkWithUnmask f = liftBaseWith $ \runInIO →
 -- @action@ and the @and_then@ function have access to the captured
 -- state, all their side-effects in @m@ are discarded. They're run
 -- only for their side-effects in 'IO'.
-forkFinally ∷ MonadBaseControl IO m
-            ⇒ m a → (Either SomeException a → m ()) → m ThreadId
+forkFinally :: MonadBaseControl IO m
+            => m a -> (Either SomeException a -> m ()) -> m ThreadId
 forkFinally action and_then =
     mask $ \restore ->
       fork $ try (restore action) >>= and_then
@@ -177,8 +177,8 @@ forkFinally action and_then =
 #endif
 
 -- | Generalized version of 'C.killThread'.
-killThread ∷ MonadBase IO m ⇒ ThreadId → m ()
-killThread = liftBase ∘ C.killThread
+killThread :: MonadBase IO m => ThreadId -> m ()
+killThread = liftBase . C.killThread
 {-# INLINABLE  killThread #-}
 
 #if MIN_VERSION_base(4,4,0)
@@ -187,8 +187,8 @@ killThread = liftBase ∘ C.killThread
 -- Note that, while the forked computation @m ()@ has access to the captured
 -- state, all its side-effects in @m@ are discarded. It is run only for its
 -- side-effects in 'IO'.
-forkOn ∷ MonadBaseControl IO m ⇒ Int → m () → m ThreadId
-forkOn = liftBaseDiscard ∘ C.forkOn
+forkOn :: MonadBaseControl IO m => Int -> m () -> m ThreadId
+forkOn = liftBaseDiscard . C.forkOn
 {-# INLINABLE forkOn #-}
 
 -- | Generalized version of 'C.forkOnWithUnmask'.
@@ -196,59 +196,59 @@ forkOn = liftBaseDiscard ∘ C.forkOn
 -- Note that, while the forked computation @m ()@ has access to the captured
 -- state, all its side-effects in @m@ are discarded. It is run only for its
 -- side-effects in 'IO'.
-forkOnWithUnmask ∷ MonadBaseControl IO m ⇒ Int → ((∀ a. m a → m a) → m ()) → m ThreadId
-forkOnWithUnmask cap f = liftBaseWith $ \runInIO →
-                           C.forkOnWithUnmask cap $ \unmask →
+forkOnWithUnmask :: MonadBaseControl IO m => Int -> ((∀ a. m a -> m a) -> m ()) -> m ThreadId
+forkOnWithUnmask cap f = liftBaseWith $ \runInIO ->
+                           C.forkOnWithUnmask cap $ \unmask ->
                              void $ runInIO $ f $ liftBaseOp_ unmask
 {-# INLINABLE forkOnWithUnmask #-}
 
 -- | Generalized version of 'C.getNumCapabilities'.
-getNumCapabilities ∷ MonadBase IO m ⇒ m Int
+getNumCapabilities :: MonadBase IO m => m Int
 getNumCapabilities = liftBase C.getNumCapabilities
 {-# INLINABLE getNumCapabilities #-}
 
 #if MIN_VERSION_base(4,6,0)
 -- | Generalized version of 'C.setNumCapabilities'.
-setNumCapabilities ∷ MonadBase IO m ⇒ Int → m ()
-setNumCapabilities = liftBase ∘ C.setNumCapabilities
+setNumCapabilities :: MonadBase IO m => Int -> m ()
+setNumCapabilities = liftBase . C.setNumCapabilities
 {-# INLINABLE setNumCapabilities #-}
 #endif
 
 -- | Generalized version of 'C.threadCapability'.
-threadCapability ∷ MonadBase IO m ⇒ ThreadId → m (Int, Bool)
-threadCapability = liftBase ∘ C.threadCapability
+threadCapability :: MonadBase IO m => ThreadId -> m (Int, Bool)
+threadCapability = liftBase . C.threadCapability
 {-# INLINABLE threadCapability #-}
 #endif
 
 -- | Generalized version of 'C.yield'.
-yield ∷ MonadBase IO m ⇒ m ()
+yield :: MonadBase IO m => m ()
 yield = liftBase C.yield
 {-# INLINABLE yield #-}
 
 -- | Generalized version of 'C.threadDelay'.
-threadDelay ∷ MonadBase IO m ⇒ Int → m ()
-threadDelay = liftBase ∘  C.threadDelay
+threadDelay :: MonadBase IO m => Int -> m ()
+threadDelay = liftBase .  C.threadDelay
 {-# INLINABLE threadDelay #-}
 
 -- | Generalized version of 'C.threadWaitRead'.
-threadWaitRead ∷ MonadBase IO m ⇒ Fd → m ()
-threadWaitRead = liftBase ∘ C.threadWaitRead
+threadWaitRead :: MonadBase IO m => Fd -> m ()
+threadWaitRead = liftBase . C.threadWaitRead
 {-# INLINABLE threadWaitRead #-}
 
 -- | Generalized version of 'C.threadWaitWrite'.
-threadWaitWrite ∷ MonadBase IO m ⇒ Fd → m ()
-threadWaitWrite = liftBase ∘ C.threadWaitWrite
+threadWaitWrite :: MonadBase IO m => Fd -> m ()
+threadWaitWrite = liftBase . C.threadWaitWrite
 {-# INLINABLE threadWaitWrite #-}
 
 #if !MIN_VERSION_base(4,6,0)
 -- | Generalized version of 'C.mergeIO'.
-merge ∷ MonadBase IO m ⇒ [a] → [a] → m [a]
+merge :: MonadBase IO m => [a] -> [a] -> m [a]
 merge xs ys = liftBase $ C.mergeIO xs ys
 {-# INLINABLE merge #-}
 
 -- | Generalized version of 'C.nmergeIO'.
-nmerge ∷ MonadBase IO m ⇒ [[a]] → m [a]
-nmerge = liftBase ∘ C.nmergeIO
+nmerge :: MonadBase IO m => [[a]] -> m [a]
+nmerge = liftBase . C.nmergeIO
 {-# INLINABLE nmerge #-}
 #endif
 
@@ -257,28 +257,28 @@ nmerge = liftBase ∘ C.nmergeIO
 -- Note that, while the forked computation @m ()@ has access to the captured
 -- state, all its side-effects in @m@ are discarded. It is run only for its
 -- side-effects in 'IO'.
-forkOS ∷ MonadBaseControl IO m ⇒ m () → m ThreadId
+forkOS :: MonadBaseControl IO m => m () -> m ThreadId
 forkOS = liftBaseDiscard C.forkOS
 {-# INLINABLE forkOS #-}
 
 -- | Generalized version of 'C.isCurrentThreadBound'.
-isCurrentThreadBound ∷ MonadBase IO m ⇒ m Bool
+isCurrentThreadBound :: MonadBase IO m => m Bool
 isCurrentThreadBound = liftBase C.isCurrentThreadBound
 {-# INLINABLE isCurrentThreadBound #-}
 
 -- | Generalized version of 'C.runInBoundThread'.
-runInBoundThread ∷ MonadBaseControl IO m ⇒ m a → m a
+runInBoundThread :: MonadBaseControl IO m => m a -> m a
 runInBoundThread = liftBaseOp_ C.runInBoundThread
 {-# INLINABLE runInBoundThread #-}
 
 -- | Generalized version of 'C.runInUnboundThread'.
-runInUnboundThread ∷ MonadBaseControl IO m ⇒ m a → m a
+runInUnboundThread :: MonadBaseControl IO m => m a -> m a
 runInUnboundThread = liftBaseOp_ C.runInUnboundThread
 {-# INLINABLE runInUnboundThread #-}
 
 #if MIN_VERSION_base(4,6,0)
 -- | Generalized versio  of 'C.mkWeakThreadId'.
-mkWeakThreadId ∷ MonadBase IO m ⇒ ThreadId → m (Weak ThreadId)
-mkWeakThreadId = liftBase ∘ C.mkWeakThreadId
+mkWeakThreadId :: MonadBase IO m => ThreadId -> m (Weak ThreadId)
+mkWeakThreadId = liftBase . C.mkWeakThreadId
 {-# INLINABLE mkWeakThreadId #-}
 #endif
