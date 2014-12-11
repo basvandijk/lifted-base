3b8d612ab36704fc0b9588e8f7c4545ba5e70991
Bas van Dijk <v.dijk.bas@gmail.com>
2014-12-11 11:27:59 +0100

    Add default.nix and lifted-base.nix

---
+++ lifted-base.nix
@@ -0,0 +1,20 @@
+{ cabal, HUnit, monadControl, testFramework, testFrameworkHunit
+, transformers, transformersBase
+}:
+
+cabal.mkDerivation (self: {
+  pname = "lifted-base";
+  version = "0.2.3.0";
+  src = ./.;
+  buildDepends = [ monadControl transformersBase ];
+  testDepends = [
+    HUnit monadControl testFramework testFrameworkHunit transformers
+    transformersBase
+  ];
+  meta = {
+    homepage = "https://github.com/basvandijk/lifted-base";
+    description = "lifted IO operations from the base library";
+    license = self.stdenv.lib.licenses.bsd3;
+    platforms = self.ghc.meta.platforms;
+  };
+})
