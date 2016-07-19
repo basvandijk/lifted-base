273a8a16a6d51d465e37f25c700ce4aec16fc866
Bas van Dijk <v.dijk.bas@gmail.com>
2016-07-19 18:51:32 +0200

    Updated Nix files

--- lifted-base.nix
+++ lifted-base.nix
@@ -1,20 +1,17 @@
-{ cabal, HUnit, monadControl, testFramework, testFrameworkHunit
-, transformers, transformersBase, transformersCompat
+{ mkDerivation, base, HUnit, monad-control, stdenv, test-framework
+, test-framework-hunit, transformers, transformers-base
+, transformers-compat, criterion
 }:
-
-cabal.mkDerivation (self: {
+mkDerivation {
   pname = "lifted-base";
   version = "HEAD";
   src = ./.;
-  buildDepends = [ monadControl transformersBase ];
-  testDepends = [
-    HUnit monadControl testFramework testFrameworkHunit transformers
-    transformersBase transformersCompat
+  libraryHaskellDepends = [ base monad-control transformers-base ];
+  testHaskellDepends = [
+    base HUnit monad-control test-framework test-framework-hunit
+    transformers transformers-base transformers-compat criterion
   ];
-  meta = {
-    homepage = "https://github.com/basvandijk/lifted-base";
-    description = "lifted IO operations from the base library";
-    license = self.stdenv.lib.licenses.bsd3;
-    platforms = self.ghc.meta.platforms;
-  };
-})
+  homepage = "https://github.com/basvandijk/lifted-base";
+  description = "lifted IO operations from the base library";
+  license = stdenv.lib.licenses.bsd3;
+}
