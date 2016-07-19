763bdb10bdf6c202de876b752ee388e8a2c48508
Bas van Dijk <v.dijk.bas@gmail.com>
2016-07-19 19:00:06 +0200

    Bump dependencies

--- lifted-base.nix
+++ lifted-base.nix
@@ -1,6 +1,6 @@
 { mkDerivation, base, HUnit, monad-control, stdenv, test-framework
 , test-framework-hunit, transformers, transformers-base
-, transformers-compat, criterion
+, transformers-compat, criterion, monad-peel
 }:
 mkDerivation {
   pname = "lifted-base";
@@ -9,7 +9,10 @@ mkDerivation {
   libraryHaskellDepends = [ base monad-control transformers-base ];
   testHaskellDepends = [
     base HUnit monad-control test-framework test-framework-hunit
-    transformers transformers-base transformers-compat criterion
+    transformers transformers-base transformers-compat
+
+    # Benchmark dependencies
+    criterion monad-peel
   ];
   homepage = "https://github.com/basvandijk/lifted-base";
   description = "lifted IO operations from the base library";
